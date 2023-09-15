2af0dc607b5332c28dd7552d50a279daf6d99cc6
Francis Cawog <Franciscawog@gmail.com>
2023-09-15 15:15:08 -0400

    finished my_transpose

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
@@ -20,7 +20,36 @@ class Array
         end
         return pairs
     end
+
+    def my_transpose
+        multidimensional = false
+        self.each do |ele|
+            if ele.is_a?(Array)
+                multidimensional = true
+            end
+        end
+
+        raise 'Input array must be 2D array' if multidimensional == false
+
+        new_arr = []
+        i = 0
+
+        while i < self.length
+            self.each do |subarray|
+                new_arr << subarray[i]
+            end
+            i += 1
+        end
+
+        new_arr.each_slice(self.length).to_a
+    end
 end
 
-p [1, 2, 1, 3, 3].my_uniq
-p [-1, 0, 2, -2, 1].two_sum
+p ([
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8]
+]).my_transpose
+ # => [[0, 3, 6],
+ #     [1, 4, 7],
+ #     [2, 5, 8]]
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
@@ -37,3 +37,17 @@ describe '#two_sum' do
     expect([-1, 0, 2, 0, -2, 1].two_sum).to eq([[0, 5], [1, 3], [2, 4]])
   end
 end
+
+describe "#my_transpose" do
+  it "should accept a 2D array as an argument" do
+    expect{[[0, 1, 2], [3, 4, 5], [6, 7, 8]].my_transpose}.to_not raise_error
+  end
+
+  it "should not accept an arguement that is not a 2D array" do
+    expect {[1,2,3,4,5,6,7,8,9].my_transpose}.to raise_error("Input array must be 2D array")
+  end
+
+  it "should transpose the array" do
+    expect([[0, 1, 2], [3, 4, 5], [6, 7, 8]].my_transpose).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
+  end
+end
