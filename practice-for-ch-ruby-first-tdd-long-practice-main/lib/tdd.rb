30cc2462c52fd28cdf8c6d131d0566f5a8b3509c
Francis Cawog <Franciscawog@gmail.com>
2023-09-15 14:19:20 -0400

    finished ny_uniq

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
@@ -1,1 +1,13 @@
-fewjkfgoewiuyfeofoewffo
+class Array
+    def my_uniq
+        uniq_ele = []
+
+        self.each do |ele|
+            uniq_ele << ele if !uniq_ele.include?(ele)
+        end
+
+        uniq_ele
+    end
+end
+
+p [1, 2, 1, 3, 3].my_uniq
